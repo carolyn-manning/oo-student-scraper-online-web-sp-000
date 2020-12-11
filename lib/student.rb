2f2a7a69dcc9d0a4f226f50dfe6944c94bdd178d
Carolyn Manning <carolyn.manning3@gmail.com>
2020-12-11 04:52:04 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,7 +10,7 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    Student.new(scrape_index_page(index_url)
+    Student.new(students_array)
   end
 
   def add_student_attributes(attributes_hash)
